e1bf3f1c70c59bf39a40f746610dc5474f8160b4
eric.schoettle <eric.schoettle@gmail.com>
2017-05-12 13:24:16 -0700

    make not_stores and not_brands functions

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,4 +1,10 @@
 class Brand < ActiveRecord::Base
   has_many :joins
   has_many :stores, through: :joins
+
+  def not_stores
+    all_stores = Store.all()
+    brand_stores = self.stores()
+    not_stores = all_stores - brand_stores
+  end
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,8 @@
 class Store < ActiveRecord::Base
   has_many :joins
   has_many :brands, through: :joins
+
+  def not_brands
+    return not_brands = Brand.all() - self.brands()
+  end
 end
