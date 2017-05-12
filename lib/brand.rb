c7eeaffd181fec70d2d1b44bbf0360897a5c14e8
eric.schoettle <eric.schoettle@gmail.com>
2017-05-12 16:38:38 -0700

    add validation checks

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,6 +1,9 @@
 class Brand < ActiveRecord::Base
   has_many :joins, dependent: :destroy
   has_many :stores, through: :joins
+  validates :name, uniqueness: { case_sensitive: false }
+  validates :name, length: { maximum: 100, too_long: "100 characters is the maximum allowed" }
+  validates :name, presence: true
 
   def not_stores
     all_stores = Store.all()
@@ -8,3 +11,5 @@ class Brand < ActiveRecord::Base
     not_stores = all_stores - brand_stores
   end
 end
+
+# :length => { :maximum => 100 },
--- lib/store.rb
+++ lib/store.rb
@@ -1,8 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :joins, dependent: :destroy
   has_many :brands, through: :joins
+  validates :name, uniqueness: { case_sensitive: false }
+  validates :name, length: { maximum: 100, too_long: "100 characters is the maximum allowed" }
+  validates :name, presence: true
 
   def not_brands
     return not_brands = Brand.all() - self.brands()
   end
 end
+
+# :length => { :maximum => 100 },
