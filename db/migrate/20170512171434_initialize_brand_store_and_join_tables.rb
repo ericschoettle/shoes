49a8499db2b367768966e7b43f69963e2d874706
eric.schoettle <eric.schoettle@gmail.com>
2017-05-12 10:32:32 -0700

    create tables in database and database schema

--- db/migrate/20170512171434_initialize_brand_store_and_join_tables.rb
+++ db/migrate/20170512171434_initialize_brand_store_and_join_tables.rb
@@ -1,4 +1,17 @@
 class InitializeBrandStoreAndJoinTables < ActiveRecord::Migration[5.1]
   def change
+    create_table :brands do |t|
+      t.column :name, :string
+      t.column :price, :integer
+    end
+
+    create_table :joins do |t|
+      t.column :brand_id, :integer
+      t.column :store_id, :integer
+    end
+
+    create_table :stores do |t|
+      t.column :name, :string
+    end
   end
 end
