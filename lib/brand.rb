d288f15a0a2aaf0b1499a3e42767429952d1d449
eric.schoettle <eric.schoettle@gmail.com>
2017-05-12 16:53:36 -0700

    add capitalization callback

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,15 +1,21 @@
 class Brand < ActiveRecord::Base
   has_many :joins, dependent: :destroy
   has_many :stores, through: :joins
+
   validates :name, uniqueness: { case_sensitive: false }
   validates :name, length: { maximum: 100, too_long: "100 characters is the maximum allowed" }
   validates :name, presence: true
 
-  def not_stores
-    all_stores = Store.all()
-    brand_stores = self.stores()
-    not_stores = all_stores - brand_stores
-  end
-end
+  before_save(:capitalize_words)
 
-# :length => { :maximum => 100 },
+  def not_stores
+    not_stores = Store.all() - self.stores()
+  end
+
+private
+  def capitalize_words
+    binding.pry
+    caps = self.name().split(' ').map(&:capitalize).join(' ')
+  end
+
+end
--- lib/store.rb
+++ lib/store.rb
@@ -1,13 +1,21 @@
 class Store < ActiveRecord::Base
   has_many :joins, dependent: :destroy
   has_many :brands, through: :joins
+
   validates :name, uniqueness: { case_sensitive: false }
   validates :name, length: { maximum: 100, too_long: "100 characters is the maximum allowed" }
   validates :name, presence: true
 
+  before_save(:capitalize_words)
+
   def not_brands
     return not_brands = Brand.all() - self.brands()
   end
-end
 
-# :length => { :maximum => 100 },
+  private
+    def capitalize_words
+      binding.pry
+      self.name = name.split(' ').map(&:capitalize).join(' ')
+    end
+
+end
